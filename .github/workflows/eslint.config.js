6a46fe0f814140578333a145a5993e48d4c8af0b
Abir Kheireddine <kheireddineabir@gmail.com>
2025-01-08 11:56:56 +0100

    added eslint.config.js + re-installed eslint

---
+++ .github/workflows/eslint.config.js
@@ -0,0 +1,17 @@
+export default [
+    {
+      ignores: ['node_modules/**'],
+    },
+    {
+      files: ['**/*.js'],
+      languageOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      rules: {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+      },
+    },
+  ];
+  
